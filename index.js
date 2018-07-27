e92d2c4ce406072640c3c0f45912c6542c3403de
Kaveh Saleminejad <lcaveh@gmail.com>
2018-07-27 19:08:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var recipes = { egg : "3 pcs", milk : "one cup"};
 function updateObjectWithKeyAndValue(object, key, value) {
-  return object.assign ({key : value}, object);
+  return Object.assign ({key : value}, object);
 }
